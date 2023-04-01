bf82c63a3753c2f319ebd34736646eed55ba9dab
John Loyd Mulit <jlmulit68@gmail.com>
2023-04-01 14:36:15 +0800

    setup db tables and add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ClerkProvider {...pageProps}>
+            <div className="w-full mx-auto max-w-3xl bg-blue-300">
+                <Head>
+                    <title>Expenses Tracker</title>
+                </Head>
+                <Component {...pageProps} />
+            </div>
+        </ClerkProvider>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 export default function Home() {
     return (
-        <div className="w-full mx-auto max-w-3xl">
-            <h1>Home</h1>
+        <div className="">
+            <header className="p-2">
+                <SignedIn>
+                    <div className="flex justify-end">
+                        <UserButton />
+                    </div>
+                </SignedIn>
+                <SignedOut>
+                    <div className="flex justify-end">
+                        <SignInButton mode="modal">
+                            <button className="bg-red-400 p-1 rounded-md">
+                                Sign In
+                            </button>
+                        </SignInButton>
+                    </div>
+                </SignedOut>
+            </header>
+            <main>{/* main content here */}</main>
         </div>
     )
 }
-
