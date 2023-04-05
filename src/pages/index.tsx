434a4a64142df7cfc66a99abeb1c5aa0036273a9
John Loyd Mulit <jlmulit68@gmail.com>
2023-04-05 14:58:12 +0800

    slightly revamp landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import { Anchor, Button, Center, Container, Flex, Title } from "@mantine/core";
 import Link from "next/link";
 
 export default function Home() {
 
     return (
-        <>
-            <h1> Landing Page </h1>
-            <Link href={'/home'} className="bg-red-300 p-1 rounded-md">Get started</Link>
-        </>
+        <Container size={'sm'} p={'sm'}>
+            <Title align="center"> Landing Page </Title>
+            <Flex justify={'center'} mt={'sm'}>
+                <Link href={'/home'}>
+                    <Button style={{ backgroundColor: 'blue' }}>Get started</Button>
+                </Link>
+            </Flex>
+        </Container>
     )
 }
 
